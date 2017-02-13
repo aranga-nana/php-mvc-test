0460763d3b4ae974801c2bff5470014d9d0aae2c
aranga-au <aranga.nanayakkara@gmail.com>
2017-02-13 21:50:45 +1100

    fix docker file to include default site option

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-docker build . -t aranga/php
+#!/usr/bin/env bash
+docker build . -t aranga/php-app-1
 docker stop php-app
 docker rm php-app
-docker run --name php-app -p 80:80 -d aranga/php
+docker run --name php-app -p 80:80 -d aranga/php-app-1
